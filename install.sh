8d7b3b4fd8d872853f2bab353c69802299da836f
Alessandro Arnone <arnone.alessandro@gmail.com>
2026-02-05 21:19:05 +0000

    Fix broken pipe error by auto-detecting need for sudo in install script

--- install.sh
+++ install.sh
@@ -4,7 +4,12 @@ set -e
 INSTALL_DIR="/usr/local/bin"
 REPO_URL="https://raw.githubusercontent.com/aarnone/git-clone-worktree/master/git-clone-worktree"
 
-curl -fsSL "$REPO_URL" -o "$INSTALL_DIR/git-clone-worktree"
-chmod +x "$INSTALL_DIR/git-clone-worktree"
+SUDO=""
+if [ ! -w "$INSTALL_DIR" ]; then
+  SUDO="sudo"
+fi
+
+$SUDO curl -fsSL "$REPO_URL" -o "$INSTALL_DIR/git-clone-worktree"
+$SUDO chmod +x "$INSTALL_DIR/git-clone-worktree"
 
 echo "git-clone-worktree installed to $INSTALL_DIR/git-clone-worktree"
